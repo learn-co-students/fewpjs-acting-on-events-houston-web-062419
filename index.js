c10376197495618a8555f843f535d703878e0338
Aaron Briscoe <abriscoe2008@gmail.com>
2019-11-22 02:04:24 -0600

    Done.

--- index.js
+++ index.js
@@ -10,20 +10,22 @@ function moveDodgerLeft() {
     }
 };
 
-document.addEventListener('keydown', function(e){
-    if(e.key === 'ArrowLeft'){
-        moveDodgerLeft();
-    }else if(e.key === 'ArrowRight'){
-        moverDodgerRight()
-    }
-});
-
 function moveDodgerRight(){
     let leftNums = dodger.style.left.replace('px', '');
     let left = parseInt(leftNums, 10);
 
     if(left > 0){
-        dodger.style.left = `${ left + 1 }px`;
+        dodger.style.left = `${left + 1}px`;
     }
 };
+document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight()
+    }
+});
 
+document.addEventListener('keydown', function(e){
+    if(e.key === 'ArrowLeft'){
+        moveDodgerLeft();
+    }
+});
